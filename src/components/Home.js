f6efbc5c39271d5309a098bd25b62c40dc92e4ce
Tia4130 <Tiaakiki12@gmail.com>
2024-09-14 20:02:36 +0300

    setting up the Project

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//CSS
+import '../css/home.css';
+
+function Home() {
+    return (
+        <div>Home</div>
+    )
+}
+
+export default Home
---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+//CSS
+import "../css/searchBar.css";
+
+function SearchBar() {
+    return (
+        <div>SearchBar</div>
+    )
+}
+
+export default SearchBar
